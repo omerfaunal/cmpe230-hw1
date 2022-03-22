6a17a5b8efb9c838dcd080a46b79ac82631012bf
edonmez01 <edonmez2001@gmail.com>
2022-03-22 23:51:06 +0300

    Took input and separated it to its lines, added Matrix struct.

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, Eren!\n");
+struct Matrix {  // Note that scalars are considered 1 x 1 matrices and vectors are considered n x 1 matrices.
+    int row_count;
+    int column_count;
+    int *begin;  // This points to the position (1, 1) of the matrix.
+};
+
+int main(int argc, char *argv[]) {
+    FILE *fp;
+    char line[256];
+
+    if(argc != 2) {
+        printf("Give filename as as command line argument.\n");
+        return 1;
+    }
+
+    fp = fopen(argv[1], "r");
+    if(fp == NULL) {
+        printf("Cannot open %s\n", argv[1]);
+        return 1;
+    }
+
+    while(fgets(line, 256, fp) != NULL) {
+        printf("%s", line);
+    }
+
+    fclose(fp);
     return 0;
 }
