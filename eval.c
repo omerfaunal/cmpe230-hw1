0e9173caf97278bf6b6142beb5fa5574e5f52ce5
edonmez01 <edonmez2001@gmail.com>
2022-04-02 14:13:18 +0300

    Refactored because of function name change.

--- eval.c
+++ eval.c
@@ -7,8 +7,7 @@
 #define MAX_CHAR 256
 
 void error(int line);
-char* scalarValueDeclaration(char* out, char* variableName, float value);
-char* matrixDeclaration(char* out, char* variableName, int columnCount, int rowCount);
+char* declaration(char* out, char* variableName, int columnCount, int rowCount);
 char **rpn(char **line, char **out, short int start_index, short int end_index);
 int *get_dimensions(char *name, int *dimensions);
 int *typecheck(char **line, int size, char **ptranslated);
@@ -40,7 +39,7 @@ char* eval(char **line, short int size) {  // Note that size also contains the n
             return "error";
         }
         char out[MAX_CHAR + 50];
-        return scalarValueDeclaration(out, line[1], 0);
+        return declaration(out, line[1], 0, 0);
     }
 
     // Vector declaration
@@ -56,7 +55,7 @@ char* eval(char **line, short int size) {  // Note that size also contains the n
         }
         char out[MAX_CHAR + 50];
         int row_count = atoi(line[3]);
-        return matrixDeclaration(out, line[1], 1, row_count);
+        return declaration(out, line[1], 1, row_count);
     }
 
     // Matrix declaration
@@ -71,7 +70,7 @@ char* eval(char **line, short int size) {  // Note that size also contains the n
             return "error";
         }
         char out[MAX_CHAR + 50];
-        return matrixDeclaration(out, line[1], atoi(line[5]), atoi(line[3]));
+        return declaration(out, line[1], atoi(line[5]), atoi(line[3]));
     }
 
 
